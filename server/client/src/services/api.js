aea9716e6ec125ec1b1e4cbbfa8318f4ab4ddcbc
rajan2607 <rajanrajrx2001@gmail.com>
2026-02-11 02:05:02 +0530

    Update frontend API to production backend

--- server/client/src/services/api.js
+++ server/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://smart-appointment-backend-6vc8.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
